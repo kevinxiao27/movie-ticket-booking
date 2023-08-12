feca3d1b18008a671bf9379b68835c79cb41bdb3
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-08-12 13:20:26 -0600

    Added login method to user-controller and added to user-routes

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,68 +1,116 @@
 import User from "../models/User";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 export const getAllUsers = async (req, res, next) => {
-    let users
-    try {
-        users = await User.find()
-    } catch (err) {
-        return console.log(err)
-    } if (!users) {
-        return res.status(500).json({ message: "Unexpected Error Occured" })
-        // 500 is failure
-    }
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!users) {
+    return res.status(500).json({ message: "Unexpected Error Occured" });
+    // 500 is failure
+  }
 
-    return res.status(200).json({ users })
-    //200 is success
-    // syntax { users } does not need to specify {users: user} because it is es6
-}
+  return res.status(200).json({ users });
+  //200 is success
+  // syntax { users } does not need to specify {users: user} because it is es6
+};
 
 export const addUser = async (req, res, next) => {
-    const { name, email, password } = req.body
-    if (!name &&
-        name.trim() === "" &&
-        !email &&
-        email.trim() === "" &&
-        !password &&
-        password.trim() === ""
-    ) {
-        return res.status(422).json({ message: "Invalid Inputs" })
-        // 422 = unprocessable entity
-    }
-    const hashedPassword = bcrypt.hashSync(password)
-    let user
-    try {
-        user = new User({ name, email, password: hashedPassword })
-        user = await user.save()
-    } catch (err) {
-        return console.log(err)
-    }
-    if (!user) {
-        return res.status(500).json({ message: "Unexpected Error Occured" })
-    }
-    return res.status(201).json({ user })
-}
+  const { name, email, password } = req.body;
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid Inputs" });
+    // 422 = unprocessable entity
+  }
+  const hashedPassword = bcrypt.hashSync(password);
+  let user;
+  try {
+    user = new User({ name, email, password: hashedPassword });
+    user = await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Unexpected Error Occured" });
+  }
+  return res.status(201).json({ user });
+};
 export const updateUser = async (req, res, next) => {
-    const id = req.params.id
-    const { name, email, password } = req.body
-    if (!name &&
-        name.trim() === "" &&
-        !email &&
-        email.trim() === "" &&
-        !password &&
-        password.trim() === ""
-    ) {
-        return res.status(422).json({ message: "Invalid Inputs" })
-    }
+  const id = req.params.id;
+  const { name, email, password } = req.body;
+  if (
+    !name &&
+    name.trim() === "" &&
+    !email &&
+    email.trim() === "" &&
+    !password &&
+    password.trim() === ""
+  ) {
+    return res.status(422).json({ message: "Invalid Inputs" });
+  }
 
-    const hashedPassword = bcrypt.hashSync(password)
-    let user
-    try {
-        user = await User.findByIdAndUpdate(id, { name, email, password: hashedPassword })
-    } catch (errr) {
-        return console.log(errr)
-    }
-    if (!user) {
-        return res.status(500).json({ message: "Something Unexpected Occured" })
-    }
-    res.status(200).json({ message: "User Updated Successfully" })
-}
+  const hashedPassword = bcrypt.hashSync(password);
+  let user;
+  try {
+    user = await User.findByIdAndUpdate(id, {
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (errr) {
+    return console.log(errr);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Something Unexpected Occured" });
+  }
+  res.status(200).json({ message: "User Updated Successfully" });
+};
+
+export const deleteUser = async (req, res, next) => {
+  const id = req.params.id;
+  let user;
+  try {
+    user = await User.findByIdAndRemove(id);
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!user) {
+    return res.status(500).json({ message: "Something Unexpected Occured" });
+  }
+  res.status(200).json({ message: "User Deleted Successfully" });
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email && email.trim() === "" && !password && password.trim() === "") {
+    return res.status(422).json({ message: "Invalid Inputs" });
+  }
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!existingUser) {
+    return res.status(404).json({ message: "Unable to find existing user" });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res
+      .status(400)
+      .json({ message: "Password or username does not match" });
+  }
+
+  return res.status(200).json({ message: "Logged in successfully" });
+};
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { addUser, getAllUsers, updateUser } from "../controllers/user-controller"
+import { addUser, deleteUser, getAllUsers, login, updateUser } from "../controllers/user-controller"
 
 const userRouter = express.Router()
 
 userRouter.get("/", getAllUsers)
 userRouter.post("/signup", addUser)
 userRouter.put("/:id", updateUser)
+userRouter.delete("/:id", deleteUser)
+userRouter.post("/login", login)
 
 export default userRouter
