54ce1f8e8435fd4e9bf2fb94b76a9a2184adf89e
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2023-10-09 23:42:37 -0700

    Completed Search Bar and header in frontend, added axios and api-helpers. Related to front-end with useState. Changed node version to 18.18.0

---
+++ movies/src/api-helpers/api-helpers.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+export const getAllMovies = async () => {
+  const res = await axios
+    .get("http://localhost:5243/movie")
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("No Data");
+  }
+
+  const data = await res.data;
+  return data;
+};
--- movies/src/components/Header.js
+++ movies/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AppBar,
   Autocomplete,
@@ -9,24 +9,32 @@ import {
   Tab,
 } from "@mui/material";
 import MovieIcon from "@mui/icons-material/Movie";
+import { getAllMovies } from "../api-helpers/api-helpers";
 const dummyArray = ["abc", "your mother", "LMAO"];
 const Header = () => {
   const [value, setValue] = useState(0);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => setMovies(data.movies))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <AppBar>
+    <AppBar sx={{ bgcolor: "#2b2d42" }}>
       <Toolbar>
         <Box width={"20%"}>
           <MovieIcon />
         </Box>
-        <Box width={"50%"} margin={"1%"}>
+        <Box width={"50%"} margin={"auto"}>
           <Autocomplete
             freeSolo
-            options={dummyArray.map((option) => option)}
+            options={movies && movies.map((option) => option.title)}
             renderInput={(params) => (
               <TextField
+                sx={{ color: "white" }}
                 variant="standard"
                 {...params}
-                label="Search across multiple movies"
+                placeholder="Search across multiple movies"
               />
             )}
           />
